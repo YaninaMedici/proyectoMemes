4e8cc955e44f71ad87fb9bb5820a83d13f676888
YaniMedici <mediciyanina@gmail.com>
2022-06-26 23:32:53 -0300

    aside texto - cambios en contenedor meme

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// alert('Estoy funcionando')
+const textoSuperior = document.getElementById('texto-superior');
+const inputTextoSuperior = document.getElementById('input-texto-superior');
+const textoInferior = document.getElementById('texto-inferior');
+const inputTextoInferior = document.getElementById('input-texto-inferior');
+const inputCkeckboxTextoSuperior = document.getElementById('input-ckeckbox-texto-superior');
+
+// const memeTextoSuperior = document.getElementById('meme-texto-superior');
+// const memeImagen = document.getElementById('meme-imagen');
+// const memeTextoInferior = document.getElementById('meme-texto-inferior')
+
+
+
+inputTextoSuperior.addEventListener('input', (event) => {
+    const textoIngresado = event.target.value;
+    textoSuperior.innerHTML = textoIngresado;
+})
+
+inputTextoInferior.addEventListener('input', (event) => {
+    const textoIngresado = event.target.value;
+    textoInferior.innerHTML = textoIngresado;
+})
+
+// inputCkeckboxTextoSuperior.addEventListener('click', (event) => {
+
+// })
+
+
