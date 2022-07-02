f51f2e37369263e70dade9aa4d612a2b750fec54
FEstrada-0987 <florencia.estrada@hotmail.com>
2022-07-02 20:56:47 -0300

    update/AsideImagen create URL y create de funcionalidades de todos los filtros

--- app.js
+++ app.js
@@ -43,54 +43,68 @@ const cambioDeFuente = () => {
 
 
 
-
+/* ***************
+Modo oscuro agregue este comentario nada mas-DATE 1 de julio
+*************** */
 
 
 const btnSwitch = document.querySelector(`#switch`);
 btnSwitch.addEventListener('click',() => {
+    document.getElementById(`darki`).classList.toggle(`dark`);
     document.getElementById(`nav`).classList.toggle(`dark`);  
-    document.getElementById(`section-principal`).classList.toggle(`dark`);     
-    document.getElementById(`imagen-aside`).classList.toggle(`dark`); 
-    document.getElementById(`texto-aside`).classList.toggle(`dark`);    
+    document.getElementById(`section`).classList.toggle(`dark`);     
+   document.getElementById(`imagen-aside`).classList.toggle(`dark`); 
+   document.getElementById(`texto-aside`).classList.toggle(`dark`); 
     btnSwitch.classList.toggle(`active`);  
 });
 
+/* *** FILTROS >> ** */ 
 
-    rangeInput = document.getElementById(`Brillito`);    
+    rangeInput = document.getElementsByClassName(`input-estilo-deslizante`);    
+    // probando console.log(rangeInput)
+    meme = document.getElementById(`imagen-aqui`);       
+
+         Array.from(rangeInput).forEach(function(element) {
+            element.addEventListener('change',function(){ 
+                //Probando que funcione console.log("algo",element.name) 
+                meme.style.filter = element.name+"(" + element.value + element.title +")" 
+            })          
+          });
+
+        /* *** Empece hacer esta funcion pero la reescribi para no tener que repetir varias veces 2deJulio FE
+           
+
+     meme.style.filter = "brightness(" + rangeInput.value + "%)"            
+
+         });
+
+            rangeInputOp = document.getElementById(`opaco`);  
+
+             rangeInputOp.addEventListener("change",function(){
+           
+                  meme.style.filter = "opacity(" + rangeInputOp.value + "%)"            
+     
+              });
+
+              rangeInputCo = document.getElementById(`contrasteu`);   
+           
+                   rangeInputCo.addEventListener("change",function(){
+                 
+                        meme.style.filter = "contrast(" + rangeInputCo.value + "%)"            
+           
+                    }); **/      
+
+/* ***************
+URL  Hoy hice esta funcion para agregar la imagen al generador  2 de julio - FE
+*************** */
+
+const urlImagen = document.getElementById("url");
+
+    urlImagen.oninput = function() {
     
-    container = document.getElementsByClassName(`container`)[0];
+      // Probando que funcione con  console.log(urlImagen.value)
 
-    rangeInput.addEventListener("change",function(){
-            container.style.filter = "brightness(" + rangeInput.value + "%)"
-            
+    meme.src = urlImagen.value
 
-    });
+};
 
-
-    rangeInput = document.getElementById(`opaco`);       
-    container = document.getElementsByClassName(`container`)[0];
-
-    rangeInput.addEventListener("change",function(){
-            container.style.filter = "opacity(" + rangeInput.value + "%)"            
-
-    });
-
-    rangeInput = document.getElementById(`contrasteu`);       
-    container = document.getElementsByClassName(`container`)[0];
-
-    rangeInput.addEventListener("change",function(){
-            container.style.filter = "contrast(" + rangeInput.value + "%)"            
-
-    });
-
-    rangeInput = document.getElementById(`desenfocado`);       
-    container = document.getElementsByClassName(`container`)[0];
-
-    rangeInput.addEventListener("change",function(){
-            container.style.filter = "blur(" + rangeInput.value + "%)"            
-
-    });
-
-
-    
-    
